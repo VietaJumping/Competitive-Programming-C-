c6a5b5bdbd5460ea379dd2256ac1b9f65aec27d0
VietaJumping <91352203+VietaJumping@users.noreply.github.com>
2021-10-08 17:18:46 -0300

    Update FenwickTree.cpp

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -1,10 +1,10 @@
-//Data Structure used to calculate Q queries and C value changes where its given that Q+C = K <= 10^5
+//Data Structure used to calculate Q queries and C value changes (where Q+C = K <= 10^5) in an array of n elements.
 //Given that the array has n elements, also considering that on a Fenwick Tree we can perform both operations
-//in O(log n) (since we always divide the current integer by a power of 2) the general time complexity is O(K.log n)
+//in O(log n) (since we always decrease the current integer by a power of 2) the general time complexity is O(K.log n)
 
 //During the explanation of the structure, let v2(k) denote the largest power of 2 that divides k, note that
-//v2(k) = k&-k as k = x1x2...xt1000...000 -> v2(k) zeros and -k = 2^31 - k = ((1-x1)(1-x2)...(1-xt)0111...111) =  
-//= (1-x1)(1-x2)...(1-xt)with v2(k) zeros.
+//v2(k) = k&-k as k = x1x2...xt1000...000 -> v2(k) zeros and -k = 2^31 - k = ((1-x1)(1-x2)...(1-xt)1000...000) -> v2(k) 
+//zeros, hence proven.
 
 //To build the tree we basically let every segment of the tree (tree[i]) be the sum of array[i-v2(i)] to array[i]
 
